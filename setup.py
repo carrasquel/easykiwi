e8e70b605087c0b4714a2c675e4607a060d3f0c7
Nelson Carrasquel <carrasquel@outlook.com>
2021-07-29 08:55:27 -0400

    requirements file added

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Easykiwi",
-    version="0.1",
+    version="1.0",
     author="Nelson Carrasquel",
     author_email="carrasquel@outlook.com",
     description="Easykiwi is a Framework for Queue Messaging Application Development for Python and RabbitMQ.",
