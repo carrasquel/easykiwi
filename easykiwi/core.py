f6679c51abe0184779274fdcf9ad6c0ca929d204
Nelson Carrasquel <carrasquel@outlook.com>
2021-07-28 22:19:36 -0400

    base methods for singleton added

--- easykiwi/core.py
+++ easykiwi/core.py
@@ -5,6 +5,18 @@ from ._singleton import Singleton
 
 class Kiwi(Singleton):
 
+    def define_rpc(self):
+
+        pass
+
+    def define_task(self):
+
+        pass
+
+    def define_broadcast(self):
+
+        pass
+
     def run(self):
 
         pass
